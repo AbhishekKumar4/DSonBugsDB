b9198e31ea8d633f8e7e8e96847471c862a0b4a6
KUMAR Abhishek <abhishek@one.steria.dom>
2019-01-07 18:36:10 +0530

    adding filters for preprocessing

---
+++ filters/ticketfilters.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 07 17:22:33 2019
+@author: abhishek
+"""
+import pandas as pd
+
+def filter_assignee(assigneeStatus, dataFrame):
+    return dataFrame
+
+def filter_resolution(resolutions, dataFrame):
+    return dataFrame
