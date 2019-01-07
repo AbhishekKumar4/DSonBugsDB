9950d0275e9738152030d2e2eb1aaa423ad3a1ad
Abhishek <kumarabhishek0388@gmail.com>
2019-01-07 18:38:29 +0530

    adding comments to be implemented

--- filters/ticketfilters.py
+++ filters/ticketfilters.py
@@ -6,7 +6,9 @@ Created on Mon Jan 07 17:22:33 2019
 import pandas as pd
 
 def filter_assignee(assigneeStatus, dataFrame):
+    #add processing on dataframe
     return dataFrame
 
 def filter_resolution(resolutions, dataFrame):
+    #add processing on dataframe
     return dataFrame
